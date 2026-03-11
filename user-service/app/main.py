09cbebf291e264b88168928d9795efd8abe1d40e
Yasemin Atiş <atisyasemin01@gmail.com>
2026-03-11 13:09:19 +0300

    user servisine listeleme ve ekleme endpointleri eklendi ve test edildi

--- user-service/app/main.py
+++ user-service/app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from app.routes.user_routes import router as user_router
 
 app = FastAPI(title="User Service")
 
 @app.get("/")
 def root():
     return {"service": "user-service", "status": "running"}
+
+app.include_router(user_router)
